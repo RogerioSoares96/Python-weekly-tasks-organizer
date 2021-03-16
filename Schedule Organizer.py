400fdf2eeda1ce2aa00c0c83109c6d74e0741d24
Rogerio Barros Soares <70479058+RogerioSoares96@users.noreply.github.com>
2021-03-16 17:38:22 +0000

    Add files via upload

---
+++ Schedule Organizer.py
@@ -0,0 +1,20 @@
+import random
+import math
+def weekly_scheduler():
+
+ Tarefas = input("Weekly Scheduler, press enter...")
+ Taskers = ["Rogerio","Marilia","Moncarcha","Lopes","Gui","Valter","Kika"]; 
+ random.shuffle(Taskers)
+ Functions =[None]*len(Taskers); 
+ for i in range(0, len(Taskers)):
+     Functions[i] = Taskers[i]; 
+ bottom_floor = Functions[1]
+ stairs_toilet_bottom_floor = Functions[2]
+ floor_1_toilet = Functions[3]
+ garbadge_street = Functions[4]
+ print("Bottom Floor:" + bottom_floor)
+ print("Stairs and Toilet:" + stairs_toilet_bottom_floor)
+ print("1st Floor Toilet:" + floor_1_toilet)
+ print("Garbadge on Thursdays:" + garbadge_street)
+ weekly_scheduler()
+weekly_scheduler()
