bb8f17e4b0acb54f7d60ce7d612b4652dca4656b
Rogerio Barros Soares <70479058+RogerioSoares96@users.noreply.github.com>
2021-03-21 15:00:15 +0000

    Update Schedule Organizer.py

--- Schedule Organizer.py
+++ Schedule Organizer.py
@@ -2,16 +2,16 @@ import random
 import math
 def weekly_scheduler():
 
- Weekly_scheduler = input("Weekly Scheduler, press enter...")
- Taskers = ["Rogerio","Marilia","Moncarcha","Lopes","Gui","Valter","Kika"]; 
- random.shuffle(Taskers)
- Functions =[None]*len(Taskers); 
- for i in range(0, len(Taskers)):
-     Functions[i] = Taskers[i]; 
- bottom_floor = Functions[1]
- stairs_toilet_bottom_floor = Functions[2]
- floor_1_toilet = Functions[3]
- garbadge_street = Functions[4]
+ sched = input("Weekly Scheduler, press enter...")
+ taskers = ["1st tasker","2nd tasker","3rd","4th","5th","6th","7th"]; 
+ random.shuffle(taskers)
+ tasks =[None]*len(taskers); 
+ for i in range(0, len(taskers)):
+     tasks[i] = taskers[i]; 
+ bottom_floor = tasks[1]
+ stairs_toilet_bottom_floor = tasks[2]
+ floor_1_toilet = tasks[3]
+ garbadge_street = tasks[4]
  print("Bottom Floor:" + bottom_floor)
  print("Stairs and Toilet:" + stairs_toilet_bottom_floor)
  print("1st Floor Toilet:" + floor_1_toilet)
